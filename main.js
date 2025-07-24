7c01a92205d7b36a46762bbe605fcab7e3ebe329
luis pardo <luispardo327@gmail.com>
2025-07-23 23:17:56 -0500

    ensayando el DOM

---
+++ main.js
@@ -0,0 +1,60 @@
+   // cambiar el titulo
+    let titulo = document.getElementById('miTitulo');
+    titulo.innerText = "hola estoy editando el contenido de h1 desde javascript";
+
+    //cambiar el texto 
+    let cambiarParrafo = document.getElementById('parrafo')
+    let arr = [
+      'La practica hace al maestro',
+      'No te rindas',
+      'A programar se aprende programando '
+    ]
+    function alHacerClic(){
+      switch (parrafo.innerText){
+        case arr[0]:
+          parrafo.innerText = arr[1]
+          break
+          case arr[1]:
+          parrafo.innerText = arr[2]
+          break
+          case arr[2]:
+          parrafo.innerText = arr[0]
+          break
+          default:
+            parrafo.innerText = arr[0]
+      }
+    }
+    cambiarParrafo.addEventListener('click', alHacerClic)
+
+    // cambiar color de cada cuadro 
+
+
+    let cuadro1 = document.getElementById('cuadroOne');
+    let cuadro2 = document.getElementById('cuadroTwo');
+
+    cuadro1.addEventListener('click', cambiarColorCuadroOne);
+    cuadro2.addEventListener('click', cambiarColorCuadroTwo);
+   
+
+    function cambiarColorCuadroOne(){
+        let color1 = cuadro1.style.backgroundColor;
+        let newColor = adquirirColor(color1);
+        cuadro1.style.backgroundColor = newColor;
+    }
+
+
+      function cambiarColorCuadroTwo(){
+        let color2 = cuadro2.style.backgroundColor;
+        let newColor = adquirirColor(color2);
+        cuadro2.style.backgroundColor = newColor;
+    }
+
+    function adquirirColor(color){
+        if (color === 'gray'){
+            return 'coral'
+        }else {
+            return 'gray'
+        }
+    }
+
+
